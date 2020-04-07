552a2a18d0c3a09d226393d3251d1ad70cb88d66
Mario Mitte <mario_mitte@hotmail.com>
2020-04-07 22:42:14 +0200

    1–2d Customizing the Obtain Token Serializer and view

--- djsr/authentication/urls.py
+++ djsr/authentication/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from authentication import views as authentication_views
+
+
+
 urlpatterns = [
-    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/obtain/',
+         authentication_views.CustomTokenPairView.as_view(),
+         name='token_create'),  # override sjwt stock token
+    path('token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
--- djsr/authentication/views.py
+++ djsr/authentication/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .serializers import MyTokenObtainPairSerializer
+
+
+class CustomTokenPairView(TokenObtainPairView):
+    """
+    Send custom claims in each token by importing and
+    subclassing with the original serializer.
+    """
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
